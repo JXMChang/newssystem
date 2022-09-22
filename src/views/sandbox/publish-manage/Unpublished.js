c10f9de1e19aaf57f3ff1183f3f330c95d6a7b03
Eden <rongrong.xiao@emeet.com>
2022-09-22 20:31:24 +0800

    【新增“待发布”模块（未完）】： 1、操作功能未添加； 2、获取数据时，针对用户角色的判断不完善，应该判断权限

--- src/views/sandbox/publish-manage/Unpublished.js
+++ src/views/sandbox/publish-manage/Unpublished.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import { Button } from 'antd';
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import NewsPublish from '../../../components/publish-manage/NewsPublish';
 
 export default function Unpublished() {
+  const [dataSource, setDataSource] = useState([])
+  const roleObj = {
+    "1": "superadmin",
+    "2": "admin",
+    "3": "editor"
+  }
+  const { roleId, region, username } = JSON.parse(localStorage.getItem("token"));
+
+  useEffect(() => {
+    axios.get('news?publish=1&_expand=category').then((res) => {
+      console.log(res.data);
+      const list = res.data
+      // 如果是登录用户是超级管理员，可以检索整个list,否则把自己的和同一区域下的编辑检索出来
+      setDataSource(roleObj[roleId] === "superadmin" ? list : [
+        ...list.filter(item => item.author === username),
+        ...list.filter(item => item.region === region && roleObj[item.roleId] === "admin")
+      ])
+    })
+  }, [])
   return (
     <div>
-      Unpublished
+      <NewsPublish
+        dataSource={dataSource}
+        button={(id) =>
+          <Button type="primary">
+            发布
+          </Button>}
+      >
+        
+      </NewsPublish>
     </div>
   )
 }
